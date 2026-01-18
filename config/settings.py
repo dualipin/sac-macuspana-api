dbda61b92baf28b9e459a8e50101b7b1f6db8f97
Martin Sanchez <118947989+dualipin@users.noreply.github.com>
2026-01-18 17:51:06 -0600

    feat: Extend JWT access token lifetime from 10 to 60 minutes

--- config/settings.py
+++ config/settings.py
@@ -159,7 +159,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # JWT Config
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,  # Invalida el refresh token anterior al usarlo
